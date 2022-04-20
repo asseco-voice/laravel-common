af9395c41c65e612022b5a19850a926a209f3862
jmilotic <josip.milotic@asseco-see.hr>
2022-04-20 10:35:27 +0200

    Fix for dropping foreign keyes in SQLite

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Asseco\Common;
 
+use Illuminate\Database\Schema\Blueprint;
+use Closure;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Support\Fluent;
 use Illuminate\Support\ServiceProvider;
 
 class CommonServiceProvider extends ServiceProvider
@@ -24,5 +29,35 @@ class CommonServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/asseco-common.php' => config_path('asseco-common.php'),
         ], 'asseco-common');
+
+        $this->hotfixSqlite();
+    }
+
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite',
+            function ($connection, $database, $prefix, $config) {
+                return new class($connection, $database, $prefix, $config)
+                    extends SQLiteConnection {
+                    public function getSchemaBuilder()
+                    {
+                        if ($this->schemaGrammar === null) {
+                            $this->useDefaultSchemaGrammar();
+                        }
+
+                        return new class($this) extends SQLiteBuilder {
+                            protected function createBlueprint($table, Closure $callback = null)
+                            {
+                                return new class($table, $callback) extends Blueprint {
+                                    public function dropForeign($index)
+                                    {
+                                        return new Fluent();
+                                    }
+                                };
+                            }
+                        };
+                    }
+                };
+            });
     }
 }
